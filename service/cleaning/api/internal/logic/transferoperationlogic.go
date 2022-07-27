7d7f496ea7fe26724153140c28d9250eb1ef8d65
jackiu <jackiu1997@outlook.com>
2022-07-27 12:52:38 +0000

    fixed: transfer order

--- service/cleaning/api/internal/logic/transferoperationlogic.go
+++ service/cleaning/api/internal/logic/transferoperationlogic.go
@@ -15,7 +15,6 @@ import (
 	"cleaningservice/service/cleaning/model/order"
 
 	"github.com/zeromicro/go-zero/core/logx"
-	"google.golang.org/grpc/status"
 )
 
 type TransferOperationLogic struct {
@@ -35,29 +34,29 @@ func NewTransferOperationLogic(ctx context.Context, svcCtx *svc.ServiceContext) 
 func (l *TransferOperationLogic) TransferOperation(req *types.TransferOperationRequest) (resp *types.TransferOperationResponse, err error) {
 	uid, role, err := jwtx.GetTokenDetails(l.ctx)
 	if err != nil {
-		return nil, status.Error(500, "Invalid, JWT format error")
+		return nil, errorx.NewCodeError(500, "Invalid, JWT format error")
 	} else if role != variables.Contractor {
-		return nil, status.Error(401, "Invalid, Not contractor.")
+		return nil, errorx.NewCodeError(401, "Invalid, Not contractor.")
 	}
 
 	// Valid order
 	order_item, err := l.svcCtx.BOrderModel.FindOne(l.ctx, req.Order_id)
 	if err != nil {
-		return nil, status.Error(404, "Invalid, Order not found.")
+		return nil, errorx.NewCodeError(404, "Invalid, Order not found.")
 	}
-	if order_item.Status != order.Pending || order_item.Status != order.Working {
-		return nil, errorx.NewCodeError(401, "Order is currently unable to be transfer.")
+	if order_item.Status != order.Pending && order_item.Status != order.Working {
+		return nil, errorx.NewCodeError(401, "Order is currently unable to be transfered.")
 	}
 
 	// Valid contractor
 	if uid != order_item.ContractorId.Int64 {
-		return nil, status.Error(404, "Invalid, Order not found.")
+		return nil, errorx.NewCodeError(404, "Invalid, Order not found.")
 	}
 
 	// Get Customer details
 	customer_item, err := l.svcCtx.BCustomerModel.FindOne(l.ctx, order_item.CustomerId)
 	if err != nil {
-		return nil, status.Error(404, "Invalid, Customer not found.")
+		return nil, errorx.NewCodeError(404, "Invalid, Customer not found.")
 	}
 
 	// Create operation record
@@ -70,7 +69,7 @@ func (l *TransferOperationLogic) TransferOperation(req *types.TransferOperationR
 
 	_, err = l.svcCtx.BOperationModel.Insert(l.ctx, &operation_item)
 	if err != nil {
-		return nil, status.Error(500, err.Error())
+		return nil, errorx.NewCodeError(500, err.Error())
 	}
 
 	// Transfer
@@ -80,11 +79,12 @@ func (l *TransferOperationLogic) TransferOperation(req *types.TransferOperationR
 	order_item.Status = order.Transfering
 	err = l.svcCtx.BOrderModel.Update(l.ctx, order_item)
 	if err != nil {
-		return nil, status.Error(500, err.Error())
+		logx.Info("test here")
+		return nil, errorx.NewCodeError(500, err.Error())
 	}
 
 	// TODO: Signal tranfer order
-	orderqueue.OrderTransferStart(order_item.OrderId, customer_item.CustomerPhone)
+	go orderqueue.OrderTransferStart(order_item.OrderId, customer_item.CustomerPhone)
 
 	return &types.TransferOperationResponse{
 		Code: 200,
